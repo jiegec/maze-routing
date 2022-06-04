d946aad8316534ae922de1f50065e7ccea21eaaf
Jiajie Chen <c@jia.je>
2022-06-04 23:51:06 +0800

    Fix vite outside allowing list error

--- visualizer/vite.config.js
+++ visualizer/vite.config.js
@@ -10,5 +10,10 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  server: {
+      fs: {
+          allow: ['..']
+      }
   }
 })
