e5d00e9bb742321fa8d25f10250fce163330338a
Jiajie Chen <c@jia.je>
2026-03-31 10:26:11 +0800

    Bump visualizer dependencies: vue, vite, @vitejs/plugin-vue to latest; add vite-plugin-wasm for Vite 8 compatibility

--- visualizer/vite.config.js
+++ visualizer/vite.config.js
@@ -2,10 +2,11 @@ import { fileURLToPath, URL } from 'url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import wasm from 'vite-plugin-wasm'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [wasm(), vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
